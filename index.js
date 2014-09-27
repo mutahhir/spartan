99b06ecaebe4271e23474ebd5ece8cdda1df40f8
Mutahhir <mutahhir.ali@gmail.com>
2014-09-27 17:23:34 +0500

    Return /blog/index.html on root

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-var express = require('express');
-
+var express = require('express'),
+	fs = require('fs'),
+	path = require('path');
 
 var app = express();
 
-
 app.get('/', function (req, res) {
-	res.send('Hello world');
+	fs.readFile(path.join(__dirname, 'blog', 'index.html'), 'utf8', function (err, data) {
+		if (err) {
+			return res.send(500, 'Unable to find index.html file');
+		}
+
+		return res.send(data);
+	});
 });
 
 var server = app.listen(3000, function () {
