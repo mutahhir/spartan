d7d18085cbd4ba1228cb320c76d1ea43907b4553
Mutahhir <mutahhir.ali@gmail.com>
2014-09-30 16:00:34 +0500

    Create flow for converting md posts to handlebars

--- index.js
+++ index.js
@@ -1,16 +1,95 @@
 var express = require('express'),
 	fs = require('fs'),
-	path = require('path');
+	path = require('path'),
+	expressHandlebars = require('express-handlebars'),
+	Q = require('q'),
+	MD = require('marked'),
+	frontMatter = require('json-front-matter');
 
 var app = express();
 
-app.get('/', function (req, res) {
-	fs.readFile(path.join(__dirname, 'blog', 'index.html'), 'utf8', function (err, data) {
-		if (err) {
-			return res.send(404, 'Unable to find index.html file');
-		}
+MD.setOptions({
+  renderer: new MD.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: false,
+  smartLists: true,
+  smartypants: false,
+  highlight: function (code, lang, callback) {
+    require('pygmentize-bundled')({ lang: lang, format: 'html' }, code, function (err, result) {
+      callback(err, result.toString());
+    });
+  }
+});
 
-		return res.send(data);
+app.engine('handlebars', expressHandlebars({
+	layoutsDir: 'blog/views/layouts',
+	partialsDir: 'blog/views/partials',
+	defaultLayout: 'index'
+}));
+
+app.set('view engine', 'handlebars');
+app.set('views', './blog/views');
+
+app.get('/', function (req, res) {
+	res.render('under-construction');
+});
+
+app.get('/posts/:year/:month/:day/:postName', function (req, res) {
+	// check if post-name exists in views/posts
+	var day = req.params.day,
+		month = req.params.month,
+		year = req.params.year,
+		postName = req.params.postName,
+		viewName = [year, month, day, postName].join('-'),
+		viewFilePath = path.join(__dirname, 'blog','views','posts',viewName);
+
+	fs.exists(viewFilePath + '.handlebars', function (exists) {
+		if (exists) {
+			// just serve it
+			console.log(viewFilePath, 'exists');
+			res.render('posts/'+viewName);
+		} else {
+			// check if exists under /blog/posts
+			console.log(viewFilePath, 'doesn\'t exist');
+			var mdPostFilePath = path.join(__dirname, 'blog', 'posts', viewName)+'.md';
+			fs.exists(mdPostFilePath, function (exists) {
+				if (exists) {
+					console.log(mdPostFilePath, ' exists');
+					// render to handlebars, then serve that file
+					fs.readFile(mdPostFilePath, 'utf8', function (err, data) {
+						if (err) {
+							return res.send(404, 'Unable to find post');
+						}
+
+						var frontMatterParsed = frontMatter.parse(data);
+
+						MD(frontMatterParsed.body, function (err, content) {
+							if (err) {
+								return res.send(404, 'unable to find post');
+							}
+
+							// write this as a template
+							fs.writeFile(viewFilePath + '.handlebars', content, function (err) {
+								if (err) {
+									return res.send(404, 'Unable to find post');
+								}
+
+								return res.render('posts/'+viewName);
+							});	
+						});
+					});
+				} else {
+					console.log(mdPostFilePath, 'doesn\'t exist');
+					fs.readFile(path.join(__dirname, 'public', '404.html'), 'utf8', function (err, data) {
+						return res.send(data);
+					});
+				}
+			});
+
+		}
 	});
 });
 
@@ -30,6 +109,8 @@ app.use('/static/css', express.static(path.join(__dirname, 'blog', 'styles', 'cs
 app.use('/static/img', express.static(path.join(__dirname, 'blog', 'img')));
 app.use('/static/fonts', express.static(path.join(__dirname, 'blog', 'fonts')));
 
-var server = app.listen(80, function () {
+var isDevelopment = process.env.NODE_ENV !== 'production';
+
+var server = app.listen(isDevelopment? 3000 : 80, function () {
 	console.log('Listening on port %d', server.address().port);
 });
