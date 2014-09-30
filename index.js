abcb4fe97ed98459b248691989de3308ba743e82
Mutahhir <mutahhir.ali@gmail.com>
2014-09-30 09:21:58 +0500

    Move google webmaster tools file to /public and serve it

--- index.js
+++ index.js
@@ -7,7 +7,18 @@ var app = express();
 app.get('/', function (req, res) {
 	fs.readFile(path.join(__dirname, 'blog', 'index.html'), 'utf8', function (err, data) {
 		if (err) {
-			return res.send(500, 'Unable to find index.html file');
+			return res.send(404, 'Unable to find index.html file');
+		}
+
+		return res.send(data);
+	});
+});
+
+// http://asper.me/google8ebf48e5c26a719d.html
+app.get('/google8ebf48e5c26a719d.html', function (req, res) {
+	fs.readFile(path.join(__dirname, 'public', 'google8ebf48e5c26a719d.html'), 'utf8', function (err, data) {
+		if (err) {
+			return res.send(404, 'Unable to find the google8ebf48e5c26a719d.html file');
 		}
 
 		return res.send(data);
